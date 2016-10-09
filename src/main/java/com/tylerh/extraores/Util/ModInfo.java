fba4c5155265d83e535fa5501a59bf62e59d2ebf
SpeedCuber1 <IpodTouchGuru123@Gmail.com>
2016-10-09 12:49:25 -0400

    Updated Forge

--- src/main/java/com/tylerh/extraores/Util/ModInfo.java
+++ src/main/java/com/tylerh/extraores/Util/ModInfo.java
@@ -7,7 +7,7 @@ public class ModInfo
 {
     public static final String MOD_ID = "extraores";
     public static final String MOD_NAME = "Extra Ores";
-    public static final String MOD_VERSION = "1.2";
+    public static final String MOD_VERSION = "1.12";
     public static final String CLIENT_PROXY = "com.tylerh.extraores.Proxy.ClientProxy";
     public static final String COMMON_PROXY = "com.tylerh.extraores.Proxy.CommonProxy";
 }
