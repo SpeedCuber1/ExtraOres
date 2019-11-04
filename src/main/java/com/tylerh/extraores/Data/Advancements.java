6e642d798641e9278f63e8166126a1fa72b0a1a2
SpeedCuber1 <IpodTouchGuru123@Gmail.com>
2019-11-04 06:56:40 -0500

    Added Advancement Data Generator

---
+++ src/main/java/com/tylerh/extraores/Data/Advancements.java
@@ -0,0 +1,69 @@
+package com.tylerh.extraores.Data;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.data.AdvancementProvider;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DirectoryCache;
+import net.minecraft.data.IDataProvider;
+import net.minecraft.util.ResourceLocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public class Advancements extends AdvancementProvider
+{
+    private final DataGenerator generator;
+    private static final Gson GSON = (new GsonBuilder()).setPrettyPrinting().create();
+    private static final Logger LOGGER = LogManager.getLogger();
+    private final List<Consumer<Consumer<Advancement>>> advancements = ImmutableList.of(new ExOreAdvancement());
+    public Advancements(DataGenerator generator)
+    {
+        super(generator);
+        this.generator = generator;
+    }
+    @Override
+    public void act(DirectoryCache cache)
+    {
+        Path path = this.generator.getOutputFolder();
+        Set<ResourceLocation> set = Sets.newHashSet();
+        Consumer<Advancement> consumer = (p_204017_3_) -> {
+            if (!set.add(p_204017_3_.getId())) {
+                throw new IllegalStateException("Duplicate advancement " + p_204017_3_.getId());
+            } else {
+                Path path1 = getPath(path, p_204017_3_);
+
+                try {
+                    IDataProvider.save(GSON, cache, p_204017_3_.copy().serialize(), path1);
+                } catch (IOException ioexception) {
+                    LOGGER.error("Couldn't save advancement {}", path1, ioexception);
+                }
+
+            }
+        };
+
+        for(Consumer<Consumer<Advancement>> consumer1 : this.advancements) {
+            consumer1.accept(consumer);
+        }
+
+    }
+
+    private static Path getPath(Path pathIn, Advancement advancementIn) {
+        return pathIn.resolve("data/" + advancementIn.getId().getNamespace() + "/advancements/" + advancementIn.getId().getPath() + ".json");
+    }
+
+    /**
+     * Gets a name for this provider, to use in logging.
+     */
+    public String getName() {
+        return "Extra Ores Advancements";
+    }
+}
--- src/main/java/com/tylerh/extraores/Util/ModInfo.java
+++ src/main/java/com/tylerh/extraores/Util/ModInfo.java
@@ -7,6 +7,7 @@ public class ModInfo
 {
     public static final String MOD_ID = "extraores";
     public static final boolean GUI_DEFAULT_VALUE = true;
+    public static final String ADVANCEMENT_BACKGROUND = "minecraft:textures/gui/advancements/backgrounds/stone.png";
     public static final String[] blockNames = {"Aluminum", "Bismuth", "Cadmium", "Chromium", "Cobalt", "Copper", "Gallium", "Indium", "Lead", "Manganese", "Molybdenum", "Neodymium", "Nickel", "Niobium", "Palladium", "Platinum", "Rhodium", "Ruthenium", "Silver", "Technetium", "Thallium", "Tin", "Titanium", "Tungsten", "Vanadium", "Zinc", "Zirconium"};
     public static final String[] symbols = {"Al", "Bi", "Cd", "Cr", "Co", "Cu", "Ga", "In", "Pb", "Mn", "Mo", "Nd", "Ni", "Nb", "Pd", "Pt", "Rh", "Ru", "Ag", "Tc", "Tl", "Sn", "Ti", "W", "V", "Zn", "Zr"};
 }
