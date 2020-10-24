3e077a5ce8086ce189583e6739b8e3f54f51aac6
SpeedCuber1 <IpodTouchGuru123@Gmail.com>
2020-10-24 07:46:53 -0400

    Cleaned Up Code

--- src/main/java/com/tylerh/extraores/Blocks/BlockEXOre.java
+++ src/main/java/com/tylerh/extraores/Blocks/BlockEXOre.java
@@ -2,16 +2,12 @@ package com.tylerh.extraores.Blocks;
 
 import com.tylerh.extraores.Init.InitBlocks;
 import com.tylerh.extraores.Util.CreativeTabExtraOres;
-import com.tylerh.extraores.Util.ModInfo;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
