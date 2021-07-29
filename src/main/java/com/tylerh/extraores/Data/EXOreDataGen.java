3b6abe331bcec3657ce6af09339e17ad93f3a2e5
SpeedCuber1 <IpodTouchGuru123@Gmail.com>
2021-07-29 19:13:12 -0400

    Random Update

--- src/main/java/com/tylerh/extraores/Data/EXOreDataGen.java
+++ src/main/java/com/tylerh/extraores/Data/EXOreDataGen.java
@@ -1,6 +1,7 @@
 package com.tylerh.extraores.Data;
 
 import com.tylerh.extraores.Data.Advancements.Advancements;
+import com.tylerh.extraores.Data.Loot_Tables.LootTables;
 import com.tylerh.extraores.Data.Recipes.ExOreRecipe;
 import com.tylerh.extraores.Data.Tags.ExOreBlockTag;
 import com.tylerh.extraores.Data.Tags.ExOreItemTag;
@@ -16,10 +17,10 @@ public class EXOreDataGen
     public static void gatherData(GatherDataEvent event)
     {
         DataGenerator generator = event.getGenerator();
-        //generator.addProvider(new LootTables(generator));
         generator.addProvider(new Advancements(generator));
         generator.addProvider(new ExOreRecipe(generator));
         generator.addProvider(new ExOreBlockTag(generator, event.getExistingFileHelper()));
         generator.addProvider(new ExOreItemTag(generator,new ExOreBlockTag(generator, event.getExistingFileHelper()),event.getExistingFileHelper()));
+        generator.addProvider(new LootTables(generator));
     }
 }
