d2a9ea15907290225678b64fbbe5d2bfacb7c913
SpeedCuber1 <ipodtouchguru123@gmail.com>
2023-03-11 07:40:12 -0500

    Added Blockstate and Item Model Generators

--- src/main/java/com/tylerh/extraores/Data/EXOreDataGen.java
+++ src/main/java/com/tylerh/extraores/Data/EXOreDataGen.java
@@ -3,9 +3,10 @@ package com.tylerh.extraores.Data;
 import com.tylerh.extraores.Data.Advancements.Advancements;
 import com.tylerh.extraores.Data.Loot_Tables.LootTables;
 import com.tylerh.extraores.Data.Recipes.ExOreRecipe;
+import com.tylerh.extraores.Data.Rendering.ExOreBlockstateProvider;
+import com.tylerh.extraores.Data.Rendering.ExOreItemModelProvider;
 import com.tylerh.extraores.Data.Tags.ExOreBlockTag;
 import com.tylerh.extraores.Data.Tags.ExOreItemTag;
-import net.minecraft.data.DataGenerator;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,11 +17,14 @@ public class EXOreDataGen
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event)
     {
-        DataGenerator generator = event.getGenerator();
-        generator.addProvider(true,new Advancements(generator,event.getExistingFileHelper()));
+        var generator = event.getGenerator();
+        var existingFileHelper = event.getExistingFileHelper();
+        generator.addProvider(true,new Advancements(generator,existingFileHelper));
         generator.addProvider(true,new ExOreRecipe(generator));
-        generator.addProvider(true,new ExOreBlockTag(generator, event.getExistingFileHelper()));
-        generator.addProvider(true,new ExOreItemTag(generator,new ExOreBlockTag(generator, event.getExistingFileHelper()),event.getExistingFileHelper()));
+        generator.addProvider(true,new ExOreBlockTag(generator, existingFileHelper));
+        generator.addProvider(true,new ExOreItemTag(generator,new ExOreBlockTag(generator,existingFileHelper),existingFileHelper));
         generator.addProvider(true,new LootTables(generator));
+        generator.addProvider(true,new ExOreBlockstateProvider(generator,existingFileHelper));
+        generator.addProvider(true,new ExOreItemModelProvider(generator,existingFileHelper));
     }
 }
