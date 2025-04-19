0faf92bf59fcb602c21268b20a0c800b3cf779be
SpeedCuber1 <IpodTouchGuru123@Gmail.com>
2025-04-19 18:26:40 -0400

    Began Re-Working World Gen

--- src/main/java/com/tylerh/extraores/World/ExOreBiomeModifier.java
+++ src/main/java/com/tylerh/extraores/World/ExOreBiomeModifier.java
@@ -1,11 +1,17 @@
 package com.tylerh.extraores.World;
 
+import com.tylerh.extraores.Init.BiomeModifierList;
+import com.tylerh.extraores.Init.PlacedFeatureList;
 import com.tylerh.extraores.Util.ModInfo;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
 import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.common.world.BiomeModifiers;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 public class ExOreBiomeModifier
@@ -14,6 +20,18 @@ public class ExOreBiomeModifier
     {
         var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
         var biomes = context.lookup(Registries.BIOME);
+        BiomeModifierList.modifierAdamantine = registerKey("adamantineore");
+        context.register(BiomeModifierList.modifierAdamantine,new BiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeatures.getOrThrow(PlacedFeatureList.placedFeatureAdamantine)),
+                GenerationStep.Decoration.UNDERGROUND_ORES
+        ));
+        BiomeModifierList.modifierAgate = registerKey("agateore");
+        context.register(BiomeModifierList.modifierAgate,new BiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeatures.getOrThrow(PlacedFeatureList.placedFeatureAgate)),
+                GenerationStep.Decoration.UNDERGROUND_ORES
+        ));
     }
     private static ResourceKey<BiomeModifier> registerKey(String name)
     {
